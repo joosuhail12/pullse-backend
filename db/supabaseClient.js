6d2758f333b5b9fde52ae05fd3432fb6e4091a96
monitortomar <manmindertomar@gmail.com>
2025-02-28 12:14:18 +0530

    Required Changes

--- db/supabaseClient.js
+++ db/supabaseClient.js
@@ -5,4 +5,4 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-export default supabase;
+module.exports = supabase;
