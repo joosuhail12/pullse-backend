cbd293696f5d33907ac2869c1e684ed86227a3e2
devwatts <devwatts5@gmail.com>
2025-04-02 14:24:48 +0530

    Add analytics route activation in index.js
    - Included analyticsRoute in the main application activation process to enable analytics features.

---
+++ routes/analyticsRoutes/index.js
@@ -0,0 +1,29 @@
+const authMiddlewares = require('../../middlewares/auth');
+const AuthType = require('../../constants/AuthType');
+const Handler = require('../../handlers/AnalyticsHandler');
+
+async function activate(app) {
+    const handler = new Handler();
+    const base_url = '/api/analytics';
+    app.route({
+        url: base_url + '/total-open-tickets-day-wise',
+        method: 'GET',
+        name: 'Total number of open tickets day wise',
+        preHandler: authMiddlewares.checkToken(AuthType.user),
+        schema: {
+            tags: ['Analytics'],
+            summary: 'Total number of open tickets day wise',
+            query: {
+                type: 'object',
+                properties: {
+                    workspace_id: { type: 'string' },
+                },
+            },
+        },
+        handler: async (req, reply) => {
+            return handler.getTotalOpenTicketsDayWise(req, reply);
+        },
+    });
+}
+
+module.exports = { activate };
