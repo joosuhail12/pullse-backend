09e7b1f55cfb518032c6ae722dce746e1d33a09c
Dev Watts <devwatts5@gmail.com>
2025-04-23 11:55:03 +0530

    init

--- db/schema/widgetfields.sql
+++ db/schema/widgetfields.sql
@@ -1,12 +1,20 @@
-CREATE TABLE widgetfields (
+
+CREATE TABLE widgetfield (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    customObjectFields UUID[],
-    contactFields JSON[],
-    companyFields JSON[],
-    customDataFields UUID[],
-    createdBy UUID NOT NULL,
+    widgetId UUID NOT NULL REFERENCES widget(id),
+    fieldSourceType TEXT NOT NULL, -- 'customer', 'company', 'custom_field', 'custom_object_field'
+    standardFieldName TEXT NULL, -- For standard fields (e.g., 'name', 'email')
+    label TEXT NULL, -- For standard fields label
+    placeholder TEXT NULL, -- For standard fields placeholder in form 
+    customFieldId UUID NULL, -- Reference to customFields.id
+    customObjectId UUID NULL, -- Reference to customObjects.id
+    customObjectFieldId UUID NULL, -- Reference to customObjectFields.id
+    position INTEGER NOT NULL, -- Controls order in the form
+    isRequired BOOLEAN DEFAULT FALSE, -- Whether field is mandatory
+    workspaceId TEXT NOT NULL,
+    clientId TEXT NOT NULL,
+    createdBy  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    deletedAt  TIMESTAMPTZ DEFAULT NULL,
     createdAt TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    deletedAt TIMESTAMPTZ,
-    FOREIGN KEY (createdBy) REFERENCES users(id)
 );
