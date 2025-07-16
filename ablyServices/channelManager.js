8922127c9e31f95f82765836523a9243cee66ea7
ManminderT1223 <manminder.tomar@pullse.ai>
2025-07-17 03:02:57 +0530

    Chatbot assign

--- ablyServices/channelManager.js
+++ ablyServices/channelManager.js
@@ -97,7 +97,7 @@ class ChannelManager {
       if (error) throw error;
 
       const {data: chatbotProfile, error: chatbotProfileError} = await supabase
-        .from('chatbot')
+        .from('chatbots')
         .select('*')
         .eq('id', chatbotProfileId)
         .single();
